da0428178be9846e88ee0c8f2bf6da617d7a21a1
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-29 17:52:46 -0400

    final working version

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,33 @@
 let friendsArray = [
     { 
-        name: 'Jacques',
+        name: 'Jacques Clouseau',
         photo: 'https://vanillasaga.files.wordpress.com/2012/01/pink_panther.jpg',
         scores: [ '4', '4', '2', '4', '4', '4', '5', '1', '3', '2' ] 
     },
     { 
-        name: 'peter piper',
-        photo: 'https://vanillasaga.files.wordpress.com/2012/01/pink_panther.jpg',
+        name: 'Joe Dirt',
+        photo: 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2015%2F07%2Fjoe-dirt-2.jpg%3Fw%3D1482&w=1000&c=sc&poi=face&q=70',
         scores: [ '1', '2', '3', '3', '3', '4', '3', '3', '3', '3' ] 
+    },
+    { 
+        name: 'The Hamburglar',
+        photo: 'https://am24.akamaized.net/tms/cnt/uploads/2015/05/Hamburglar-570x480.jpg',
+        scores: [ '4', '3', '3', '2', '3', '4', '4', '2', '5', '1' ] 
+    },
+    {
+        name: 'Reba',
+        photo: 'https://marriedbiography.com/wp-content/uploads/2018/08/Reba-McEntire.jpg',
+        scores: [ '2', '4', '3', '5', '4', '2', '4', '3', '1', '3' ]
+    },
+    {
+        name: 'Queen Nefertiti',
+        photo: 'https://www.ancient-origins.net/sites/default/files/field/image/Nefertiti.jpg',
+        scores: [ '2', '3', '3', '1', '3', '1', '5', '3', '1', '3' ]
+    },
+    {
+        name: 'Arya Stark',
+        photo: 'https://www.telegraph.co.uk/content/dam/tv/2016/04/20/arya_trans_NvBQzQNjv4Bqeo_i_u9APj8RuoebjoAHt0k9u7HhRJvuo-ZLenGRumA.jpg?imwidth=450',
+        scores: [ '2', '2', '3', '3', '4', '3', '4', '4', '2', '1' ]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,50 +1,57 @@
 // load userData from friends.js
 const friends = require("../data/friends.js");
 
-// api routes for getting and posting user data exported for use
-
+// routes for reading and creating user data exported for use
 module.exports = (app) => {
-    
+
+    // set up api page to hold data object    
     app.get('/api/friends', (req, res) => {
         res.json(friends)
     })
-    
+
+    // matchmaking action happens on user create
     app.post('/api/friends', (req, res) => {
-        let totalDiffArr = [];
-        let newuser = req.body;
-        let userScores = newuser.scores.map( score => {
-            return parseInt(score);
-        })
-        console.log(newuser);
-        console.log(userScores);
-        
+        let newuser = req.body; //create new user record from $.post action on survey
+
+        //isolate array of user score number values
+        let userScoresArr = newuser.scores.map(num => {
+            return parseInt(num);
+        });
+                
+        let allDiffArr = []; // blank arr for holding scored val differnces
+
+        //iterate over objects from friendsArray in friends.js
         for (var i = 0 ; i < friends.length ; i++) {
-
+            // 0 sum counter for scored diff values
             let sumDiff = 0;
-            let compScores = friends[i].scores.map( score => {
-                return parseInt(score);
+            //isolate array of potential match score number values
+            let compScoresArr = friends[i].scores.map(num => {
+                return parseInt(num)
             });
-            
-            for ( var j = 0 ; j < userScores.length ; j++) {
-                sumDiff += Math.abs(userScores[j] - compScores[j]);
+            //within each iteration of friends data loop through userScores
+            for (var j = 0 ; j < userScoresArr.length ; j++) {
+                //find absolute value diff and add it to sumDiff counter
+                sumDiff += Math.abs( userScoresArr[j] - compScoresArr[j] );
             }
-
-            totalDiffArr.push(sumDiff);
-
+            // push the final total to arr | diff val index matches user index 
+            allDiffArr.push(sumDiff);
         }
 
-        //find lowest diff to find match and save the index
-        let bestMatchVal = 1000000 ;
-        let bestMatchIndex = 0;
-        for ( var i = 0 ; i < totalDiffArr.length ; i++ ) {
-            if (totalDiffArr[i] < bestMatchVal) {
-                bestMatchVal = totalDiffArr[i];
-                bestMatchIndex = i;
+        // compare results of each calculated iteration to find best match (lowest diff value)
+        let highMatchVal = 1000; // high enough number that first time always works
+        let highMatchIndex = 0; // set to zero to start and initialize numvar
+        for ( var i = 0 ; i < allDiffArr.length ; i++ ) {
+            if (allDiffArr[i] < highMatchVal ) {
+                //update values with best best match info
+                highMatchVal = allDiffArr[i];
+                highMatchIndex = i;
             }
         }
-
-        //push new user info to all users info in friends array in app/data/friends.js
+        
+        //push new user info to all users info in app/data/friends.js
         friends.push(newuser);
-        res.json(friends[bestMatchIndex]);
+
+        //return matched user data object
+        res.json(friends[highMatchIndex]);
     })
 }
